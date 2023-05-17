fd62e5403561151e64a449a277ae7fb96f9061f2
hasiki7 <hasiki@knou.ac.kr>
2023-05-17 17:49:57 +0900

    초기 세팅

---
+++ src/main/java/com/example/sutdy/SutdyApplication.java
@@ -0,0 +1,13 @@
+package com.example.sutdy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SutdyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SutdyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sutdy/SutdyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sutdy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SutdyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
